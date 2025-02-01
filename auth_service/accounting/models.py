b5785b17a21569a3b6be532bdeceb9eb59c017e5
amin masoudi <aminmasoudi2003@gmail.com>
2025-02-01 23:05:44 +0330

    add username field with validation

--- auth_service/accounting/models.py
+++ auth_service/accounting/models.py
@@ -5,27 +5,33 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.hashers import make_password
 import uuid
 
-from .validators import PhoneValidator
+from .validators import PhoneValidator, UsernameValidator
 
 # Create your models here.
 
 
 class CustomUserManager(UserManager):
     def _create_user(self, phone, email, password, force=False, **extra_fields):
+        if "username" in extra_fields:
+            extra_fields.pop("username")
 
-        assert password != None, "password is None"
+        assert password != None, "password is required"
         password = make_password(password)
-        user = self.model(phone=phone, email=email, password=password, **extra_fields)
-        if not force:
-            user.full_clean()
+        username = email or phone
+        user = self.model(
+            username=username,
+            phone=phone,
+            email=email,
+            password=password,
+            **extra_fields,
+        )
+        user.full_clean()
         user.save(using=self._db)
         return user
 
     def create_user(self, password, phone=None, email=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        if "username" in extra_fields:
-            extra_fields.pop("username")
         return self._create_user(phone, email, password, **extra_fields)
 
     def create_superuser(self, phone=None, email=None, password=None, **extra_fields):
@@ -44,6 +50,8 @@ class CustomUserManager(UserManager):
 
 class User(AbstractUser):
 
+    username_validator = UsernameValidator()
+
     id = models.UUIDField(
         _("Id"),
         primary_key=True,
@@ -61,22 +69,41 @@ class User(AbstractUser):
         null=True,
         validators=[PhoneValidator()],
     )
-    email = models.EmailField(_("Email"), max_length=254, blank=True, null=True)
-    username = None
-    REQUIRED_FIELDS = []
-    USERNAME_FIELD = "email"
+    username = models.CharField(
+        _("Username"),
+        max_length=150,
+        unique=True,
+        help_text=_("Required. in forms of phone or email."),
+        validators=[username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
 
-    @property
-    def username_type(self): ...
+    email = models.EmailField(_("Email"), max_length=254, blank=True, null=True)
+
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     def clean(self):
         if (self.email is None) and (self.phone is None):
             raise ValidationError("require email or phone")
-
         return super().clean()
 
     class Meta:
         verbose_name = _("User")
         verbose_name_plural = _("Users")
-        unique_together = [["email", "phone"]]
         db_table = "Users"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["email"],
+                condition=models.Q(email__isnull=False),
+                name="unique_non_null_email",
+            ),
+            models.UniqueConstraint(
+                fields=["phone"],
+                condition=models.Q(phone__isnull=False),
+                name="unique_non_null_phone",
+            ),
+        ]
--- auth_service/accounting/validators.py
+++ auth_service/accounting/validators.py
@@ -1,6 +1,27 @@
+import re
 from django.utils.deconstruct import deconstructible
 from django.core import validators
+from django.core import exceptions
+from django.utils.regex_helper import _lazy_re_compile
+
 
 @deconstructible
 class PhoneValidator(validators.RegexValidator):
-    regex = r"^(\+[1-9]{1}[0-9]{11})+$"
+    regex = r"^((\+98|0)9\d{9})$"
+
+
+@deconstructible
+class UsernameValidator(validators.RegexValidator):
+    regex = r"^((\+98|0)9\d{9})$"
+
+    def __call__(self, value):
+        try:
+            validators.validate_email(value)
+            return
+
+        except exceptions.ValidationError:
+            if re.match(self.regex, value) is None:
+                raise exceptions.ValidationError(
+                    "Enter a valid email address or phone number."
+                )
+            return
