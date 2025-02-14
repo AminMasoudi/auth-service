9e4c71834d11f972fdc60a414ea38c9920dbf8de
amin masoudi <aminmasoudi2003@gmail.com>
2025-02-14 21:18:21 +0330

    set user to SimpleLazyObject

--- auth_service/accounting/middleware.py
+++ auth_service/accounting/middleware.py
@@ -1,6 +1,8 @@
+from django.utils.functional import SimpleLazyObject
 from django.utils.deprecation import MiddlewareMixin
 from auth_service.utils import get_user_from_redis
 
 class RedisAuthenticationMiddleware(MiddlewareMixin):
     def process_request(self, request):
+        request.user = SimpleLazyObject(lambda: get_user_from_redis(request))
         request.user = get_user_from_redis(request)
