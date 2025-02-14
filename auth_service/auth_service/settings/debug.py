3c76cee10144cfe98db29cc08e887e7da675c9b9
amin masoudi <aminmasoudi2003@gmail.com>
2025-02-14 21:08:40 +0330

    Add django debug toolbar for debug: check not use in test

--- auth_service/auth_service/settings/debug.py
+++ auth_service/auth_service/settings/debug.py
@@ -3,8 +3,19 @@ from .base import *
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+
+INSTALLED_APPS += [
+    "debug_toolbar",
+]
+
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- auth_service/auth_service/urls.py
+++ auth_service/auth_service/urls.py
@@ -14,10 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("auth/", include("accounting.urls"))
+    path("admin/", admin.site.urls),
+    path("auth/", include("accounting.urls")),
 ]
+
+if settings.DEBUG:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+    urlpatterns += debug_toolbar_urls()
